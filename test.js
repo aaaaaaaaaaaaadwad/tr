b6e414a6347ba08d755b764b634050bd8642677b
aaaaaaaaaaaaadwad <93518585+aaaaaaaaaaaaadwad@users.noreply.github.com>
2021-11-01 09:48:46 +0000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,30 @@
+class TrExtension {
+  getInfo() {
+    return {
+      id: 'testingexample', // change this if you make an actual extension!
+      name: 'Power',
+      blocks: [
+        {
+          opcode: 'Tr',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[ONE] ** [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'First value'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Second value'
+            }
+          }
+        }
+      ]
+    };
+  }
+  Tr(args) {
+    // Note strict equality: Inputs must match exactly: in type, case, etc.
+    return args.ONE ** args.TWO;
+  }
+}
+Scratch.extensions.register(new StrictEqualityExtension());
